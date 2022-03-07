b1b2156dfb477180dedeaa89fddfd23248e3dc15
gustavoczz <gustavocauzzi2@gmail.com>
2022-03-06 21:54:08 -0300

    a

--- teste1.lua
+++ teste1.lua
@@ -1,4 +1,29 @@
+local function hasValue (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+--------------
+
+fuelOptions = {'minecraft:coal', 'minecraft:coal_block', 'minecraft:lava_bucket'}
+local function findItemInInventory () do
+    for i = 1, 16, 1 do
+        turtle.select(i)
+        itemDetails = turtle.getItemDetail()
+        if (hasValue(fuelOptions, itemDetails.name)) do
+            print("tem alguma coisa")
+        end
+    end
+end
+
+
+-- ----------------------
+
 for x = 0, 3, 1 do
-  turtle.turnLeft();
+    turtle.turnLeft();
 end
 print("teste")
