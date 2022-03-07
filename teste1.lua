30963f2caa2affb54c9790963e03c1eadc99bd77
gustavoczz <gustavocauzzi2@gmail.com>
2022-03-06 21:55:30 -0300

    a

--- teste1.lua
+++ teste1.lua
@@ -10,7 +10,7 @@ end
 --------------
 
 fuelOptions = {'minecraft:coal', 'minecraft:coal_block', 'minecraft:lava_bucket'}
-local function findItemInInventory () do
+local function findItemInInventory ()
     for i = 1, 16, 1 do
         turtle.select(i)
         itemDetails = turtle.getItemDetail()
